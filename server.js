0d57f7e84ef20fbf634682ce7915bd853e52818b
netlanches2014-lab <netlanches2014@gmail.com>
2026-08-05 03:45:00 -0300

    Update server log message and clean up comments

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Arquivos estáticos na raiz do projeto
 app.use(express.static(__dirname));
 
-// Página inicial
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Câmbio Cortez rodando na porta ${PORT}`);
+  console.log(`Servidor iniciado na porta ${PORT}`);
 });
